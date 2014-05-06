a861bf0e312d02ab3fc4abec15ce66befc568f9c
Nicolas Spilman <nicolas.spilman@bestbuy.com>
2014-05-06 10:43:13 -0600

    Simple unit test for NetKernelPlugin

--- src/test/groovy/org/netkernel/gradle/plugin/NetKernelPluginSpec.groovy
+++ src/test/groovy/org/netkernel/gradle/plugin/NetKernelPluginSpec.groovy
@@ -1,15 +1,27 @@
 package org.netkernel.gradle.plugin
 
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 class NetKernelPluginSpec extends Specification {
 
     NetKernelPlugin netKernelPlugin
 
-//    def 'uses netkernel plugin for simple build'() {
-//        when:
-//
-//        then:
-//    }
+    void setup() {
+        netKernelPlugin = new NetKernelPlugin()
+    }
+
+    def 'applies NetKernel plugin to project'() {
+        setup:
+        File projectDir = new File(NetKernelPluginSpec.getResource("/modules/basic").file)
+        Project project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+
+        when:
+        netKernelPlugin.apply(project)
+
+        then:
+        project
+    }
 
 }

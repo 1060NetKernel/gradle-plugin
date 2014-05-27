c5d77017bcc91fa3cea3ac71f1e8c36bd0b4a14d
Nicolas Spilman <nicolas.spilman@bestbuy.com>
2014-05-27 10:22:01 -0600

    Tests for InitializeDaemonDirTask

---
+++ src/test/groovy/org/netkernel/gradle/plugin/tasks/InitializeDaemonDirTaskSpec.groovy
@@ -0,0 +1,26 @@
+package org.netkernel.gradle.plugin.tasks
+
+import org.netkernel.gradle.nk.ExecutionConfig
+import org.netkernel.gradle.plugin.BasePluginSpec
+
+class InitializeDaemonDirTaskSpec extends BasePluginSpec {
+
+    def 'check installation'() {
+        setup:
+        File baseTestDirectory = getResourceAsFile('/test/initializeDaemonDirTaskSpec')
+
+        ExecutionConfig config = new ExecutionConfig('name')
+        config.directory = baseTestDirectory.absoluteFile
+        project.netkernel = [envs: ['name': config]]
+
+        InitializeDaemonDirTask initializeDaemonDirTask = createTask(InitializeDaemonDirTask)
+        initializeDaemonDirTask.configName = 'name'
+
+        when:
+        initializeDaemonDirTask.checkInstallation()
+
+        then:
+        new File(baseTestDirectory, 'etc/kernel.properties').text == 'netkernel.init.modulesdir=etc/modules.d/'
+    }
+
+}
